906945806bb32e7701aee83c34c435bfaaaf7dc6
RushingAlien <rushing27alien@gmail.com>
2022-03-09 04:31:28 +0700

    -

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,8 +8,8 @@ dnf check-update
 # install and run nginx
 dnf -y install nginx
 systemctl enable --now nginx
-dnf -y clean all
-# Zero out the rest of the free space using dd, then delete the written file.
-#dd if=/dev/zero of=/EMPTY bs=1M
-#rm -f /EMPTY
-sync
+# dnf -y clean all
+# # Zero out the rest of the free space using dd, then delete the written file.
+# #dd if=/dev/zero of=/EMPTY bs=1M
+# #rm -f /EMPTY
+# sync
