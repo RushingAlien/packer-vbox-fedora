3390f07f5cfd092507a6b3347eba179b250c6fea
RushingAlien <rushing27alien@gmail.com>
2022-03-09 02:47:44 +0700

    -

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -eux
-sudo dnf -y clean all
+dnf -y clean all
 # Zero out the rest of the free space using dd, then delete the written file.
 dd if=/dev/zero of=/EMPTY bs=1M
 rm -f /EMPTY
