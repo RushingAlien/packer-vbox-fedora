460fcdd5ecdb840bfe2b24750fe3fc43ddc4228c
RushingAlien <rushing27alien@gmail.com>
2022-03-09 03:49:00 +0700

    -

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 dnf check-update
 # install vbox guest additions
-mount -o loop,ro ~/VBoxGuestAdditions.iso /mnt/
-/mnt/VBoxLinuxAdditions.run
-umount /mnt/
-rm -f ~/VBoxGuestAdditions.iso
+# mount -o loop,ro ~/VBoxGuestAdditions.iso /mnt/
+# /mnt/VBoxLinuxAdditions.run
+# umount /mnt/
+# rm -f ~/VBoxGuestAdditions.iso
 # install and run nginx
 dnf -y install nginx
 systemctl enable --now nginx
